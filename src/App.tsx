500d5333f058ddd52d9156db053849afc90d5e16
Illia Kocherovets <illiakocherovets@gmail.com>
2024-08-29 10:03:27 +0200

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
 
 
 export const App: React.FC = () => {
@@ -9,6 +10,7 @@ export const App: React.FC = () => {
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home/>} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Home: React.FC = () => {
+
+  return (
+    <section className="hero is-primary is-fullheight-with-navbar">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title">Welcome to the Fake Store</h1>
+          <p className="subtitle">Explore our products and find the best deals!</p>
+        </div>
+      </div>
+    </section>
+  );
+};
